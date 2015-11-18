a3caf1af56a6c1f991a3d2d61e3f616de4870cb3
Henry <henry.chong@windowslive.com>
2015-11-18 21:35:46 +0000

    Initial commit of solution.

---
+++ PlutoRover/Tests/CoreTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace PlutoRover.Tests
+{
+    [TestFixture]
+    public class CoreTests
+    {
+
+        /*
+         * Implement commands that move the rover forward/backward (‘F’,’B’). The rover may
+         * only move forward/backward by one grid point, and must maintain the same heading.
+         */
+
+        [Test]
+        public void MoveRoverForward()
+        {
+
+        }
+
+        [Test]
+        public void MoveRoverBackwards()
+        {
+
+        }
+
+        /*
+         * Implement commands that turn the rover left/right (‘L’,’R’). These commands make
+         * the rover spin 90 degrees left or right respectively, without moving from its current
+         * spot         
+         */
+
+        /*
+         * Implement wrapping from one edge of the grid to another. (Pluto is a sphere after all)
+         */
+
+        /*
+         * Implement obstacle detection before each move to a new square. If a given
+         * sequence of commands encounters an obstacle, the rover moves up to the last
+         * possible point and reports the obstacle.
+         */
+
+
+
+    }
+}

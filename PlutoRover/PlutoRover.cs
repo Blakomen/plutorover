5a44abc2aaa40ca1b8cb5de3914ecd2a1ca6ab29
Henry <henry.chong@windowslive.com>
2015-11-18 22:13:01 +0000

    Rover can now move forwards (with accompanying tests)

---
+++ PlutoRover/PlutoRover.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlutoRover
+{
+    public class PlutoRover
+    {
+        //Note: we'll probably eventually need a better world-state tracking to list all the obstacles for task #4
+
+        private int WorldBoundX;
+        private int WorldBoundY;
+        public RoverState CurrentState;
+
+        public PlutoRover(RoverState initialState, int worldBoundX, int worldBoundY)
+        {
+            WorldBoundX = worldBoundX;
+            WorldBoundY = worldBoundY;
+            CurrentState = initialState;
+        }
+
+        public CommandResult MoveForward()
+        {
+            switch(CurrentState.Facing)
+            {
+                case Facing.North:
+                    CurrentState.Y++;
+                    break;
+                case Facing.South:
+                    CurrentState.Y--;
+                    break;
+                case Facing.East:
+                    CurrentState.X++;
+                    break;
+                case Facing.West:
+                    CurrentState.X--;
+                    break;
+            }
+
+            return new CommandResult { 
+                RoverState = CurrentState, 
+                Status = CommandStatus.Success, 
+                Message = String.Format("Rover moved to [{0},{1}]", CurrentState.X, CurrentState.Y) };
+        }
+    }
+
+    public class CommandResult
+    {
+        public string Message { get; set; }
+        public CommandStatus Status { get; set;}
+        public RoverState RoverState { get; set; }
+    }
+
+    public enum CommandStatus
+    {
+        Success,
+        Failure
+    }
+
+    public enum Facing
+    {
+        North,
+        South,
+        East,
+        West
+    }
+
+    public class RoverState
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Facing Facing { get; set; }
+    }
+}
--- PlutoRover/Tests/CoreTests.cs
+++ PlutoRover/Tests/CoreTests.cs
@@ -11,28 +11,71 @@ namespace PlutoRover.Tests
     public class CoreTests
     {
 
+        PlutoRover rover;
+
+        [SetUp]
+        public void SetUp() 
+        {
+            rover = new PlutoRover(new RoverState { Facing = Facing.North, X = 0, Y = 0 }, 100, 100); 
+        }
+
         /*
          * Implement commands that move the rover forward/backward (‘F’,’B’). The rover may
          * only move forward/backward by one grid point, and must maintain the same heading.
          */
-
         [Test]
-        public void MoveRoverForward()
+        public void move_rover_fowards()
         {
+            rover.MoveForward();
 
+            Assert.AreEqual(rover.CurrentState.Facing, Facing.North);
+            Assert.AreEqual(rover.CurrentState.X, 0);
+            Assert.AreEqual(rover.CurrentState.Y, 1);
         }
 
-        [Test]
-        public void MoveRoverBackwards()
-        {
+        //[Test]
+        //public void move_rover_backwards()
+        //{
+        //    //rover.MoveBackward();
 
+        //    Assert.AreEqual(rover.CurrentState.Facing, Facing.North);
+        //    Assert.AreEqual(rover.CurrentState.X, 0);
+        //    Assert.AreEqual(rover.CurrentState.Y, -1);                      //note: this will have to change when the world wraps
+        //}
+        
+        [Test]
+        public void move_rover_forwards_three_times()
+        {
+            rover.MoveForward();
+            rover.MoveForward();
+            rover.MoveForward();
+
+            Assert.AreEqual(rover.CurrentState.Facing, Facing.North);
+            Assert.AreEqual(rover.CurrentState.X, 0);
+            Assert.AreEqual(rover.CurrentState.Y, 3);
         }
+
+        //[Test]
+        //public void move_rover_forwards_and_backwards()
+        //{
+        //    RoverState initialState = rover.CurrentState;
+
+        //    rover.MoveForward();
+        //    rover.MoveBackward();
+        //}
 
         /*
          * Implement commands that turn the rover left/right (‘L’,’R’). These commands make
          * the rover spin 90 degrees left or right respectively, without moving from its current
          * spot         
          */
+
+
+        //[Test]
+        //public void MoveRoverInASquare()
+        //{
+
+        //}
 
         /*
          * Implement wrapping from one edge of the grid to another. (Pluto is a sphere after all)
